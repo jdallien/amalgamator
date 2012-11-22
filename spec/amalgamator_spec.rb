7188978ff44cb261dc068a832ac2297fb44b7cf0
Jeff Dallien <jdallien@mdlogix.com>
2012-11-21 21:06:14 -0500

    Fix up amalgamator_spec.rb for 1.9.3. Sadly removing elementor because it hasn't been maintained in ages. Testing XML with Nokogiri directly instead.

--- spec/amalgamator_spec.rb
+++ spec/amalgamator_spec.rb
@@ -1,56 +1,43 @@
 require File.join(File.dirname(__FILE__), '..', 'lib', 'jointfeed')
 require File.join(File.dirname(__FILE__), '..', 'amalgamator')
-require File.join(File.dirname(__FILE__), '..', 'vendor', 'elementor-0.0.8', 'lib', 'elementor.rb')
-require File.join(File.dirname(__FILE__), '..', 'vendor', 'elementor-0.0.8', 'lib', 'elementor', 'spec.rb')
-require 'spec'
-require 'spec/interop/test'
 require 'rack/test'
 
 set :environment, :test
 
 describe 'The main page' do
   include Rack::Test::Methods
-  include Elementor
 
   before :each do
-    @page = elements(:from => :do_get) do |tag|
-      tag.feed_url_inputs 'input.feed_url'
-      tag.submit_button 'input.submit'
-    end
+    get '/'
+    @html = Nokogiri::HTML(last_response.body)
   end
 
   def app
     Sinatra::Application
   end
 
-  def do_get
-    get '/'
-    last_response.body
-  end
-
   it "has text boxes for the feed URLs" do
-    @page.should have(2).feed_url_inputs
+     # tag.feed_url_inputs 'input.feed_url'
+    @html.css("input.feed_url").size.should == 2
   end
 
   it "has a submit button" do
-    @page.should have(1).submit_button
+     # tag.submit_button 'input.submit'
+    @html.css("input.submit").size.should == 1
   end
 end
 
 describe 'getting two joined feeds' do
   include Rack::Test::Methods
-  include Elementor
 
   before :each do
     @jointfeed = mock(JointFeed, :title       => "TITLE",
                                  :entries     => (1..3).map{ mock_entry },
                                  :description => "DESCRIPTION")
     JointFeed.should_receive(:new).and_return(@jointfeed)
-    @page = elements(:from => :do_get, :as => :xml) do |tag|
-      tag.items 'item'
-      tag.guids 'guid'
-      tag.links 'item/link'
-    end
+
+    get "/feed", 'feeds' => ["http://feed1.test/posts.xml", "http://feed2.test/posts.xml"]
+    @xml = Nokogiri::XML(last_response.body)
   end
 
   def app
@@ -65,32 +52,25 @@ describe 'getting two joined feeds' do
                   :url       => "LINK")
   end
 
-  def do_get
-    get "/feed", 'feeds' => ["http://feed1.test/posts.xml", "http://feed2.test/posts.xml"]
-    last_response.body
-  end
-
   it "should respond ok" do
-    do_get
     last_response.should be_ok
   end
 
   it "should return an RSS XML document" do
-    do_get
     last_response.content_type.should == "application/rss+xml"
   end
 
   it "should have the correct number of feed items" do
-    @page.should have(@jointfeed.entries.size).items
+    @xml.xpath("//rss/channel/item").size.should == @jointfeed.entries.size
   end
 
   it "should repeat the items' guid field in the combined feed" do
-    @page.guids.size.should == 3
-    @page.guids[0].inner_text.should == "GUID"
+    @xml.xpath("//rss/channel/item/guid").size.should == 3
+    @xml.xpath("//rss/channel/item/guid").first.inner_text.should == "GUID"
   end
 
   it "should repeat the items' link field in the combined feed" do
-    @page.links.size.should == 3
-    @page.links[0].inner_text.should == "LINK"
+    @xml.xpath("//rss/channel/item/link").size.should == 3
+    @xml.xpath("//rss/channel/item/link").first.inner_text.should == "LINK"
   end
 end
